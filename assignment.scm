1396203c46814f36d5b094c9a27bfc884ebb5f55
Andrew Ang <andrewang123@hotmail.com>
2017-11-20 14:46:53 -0800

    only need to finish primes

--- assignment.scm
+++ assignment.scm
@@ -1,21 +1,4 @@
-(define (list-primes num)
-	(cond ((= num 2) (display num))
-		  ((= num 3) (display num))
-		  ((= num 5) (display num))
-		  ((= num 7) (display num))
-		  (else 
-		  	(begin 
-		  		(cond ((= (modulo num 2) 0) (display "hey"))
-		  		  	((= (modulo num 3) 0) (display "hey"))
-		  		  	((= (modulo num 5) 0) (display "hey"))
-		  		  	((= (modulo num 7) 0) (display "hey"))
-		  		(else (begin 
-		  			(display num)
-		  			(list-primes (- num 1))
 
-		  			)
-		  		))))))
-;;;(define (convert number base))
 
 ;;; Creates a game with two computers until they guess the same num
 (define (roll-till-you-win)
@@ -35,6 +18,7 @@
 
 ) ;;; end of function
 
+
 ;;; Converts a number to base number
 (define (convert number base)
 	(let 
@@ -48,9 +32,19 @@
 	);;; end of if 
 	) ;;; end of let
 ) ;;; end of function
-		  	
+
+(define (list-primes num)
+	(display num)
+	(if(= num 2 ) 
+		(display num)
+		
+		(begin
+			(list-primes (- num 1))
+			(display num)
+			(newline)
+		);;; end of begin
+		) ;;; end of if
 
 
-		  
 
-;;; now check if those mod
+);;; end of function
